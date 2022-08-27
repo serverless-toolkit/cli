5e8b34ef80a02ed56aab2c2810a06b5697d561ff
Mike Bild <mike.bild.ext@siemens.com>
2022-08-27 10:22:13 +0200

    add bootstrapping spinner

--- cli/bootstrap.ts
+++ cli/bootstrap.ts
@@ -1,9 +1,18 @@
 import { AwsCdkExec } from 'aws-cdk-exec';
 import { realpathSync } from 'fs';
 import { join } from 'path';
+import { Spinner } from 'cli-spinner';
 
 export async function bootstrap(argv) {
-	console.log('Bootstrapping ...');
+	const spinner = new Spinner({
+		text: '%s bootstrapping AWS serverless runtime',
+		stream: process.stdout,
+		onTick: function (msg: any) {
+			this.clearLine(this.stream);
+			this.stream.write(msg);
+		}
+	});
+	spinner.start();
 
 	const cdkApp = new AwsCdkExec({
 		appCommand: join(realpathSync(__filename), '..', '..', '..', '.build/stacks/app.js')
