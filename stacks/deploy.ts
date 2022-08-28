734957a4684281c01a89924bb2da2b90213fc5bf
Mike Bild <mike.bild.ext@siemens.com>
2022-08-28 19:08:49 +0200

    fix env vars

--- stacks/deploy.ts
+++ stacks/deploy.ts
@@ -4,11 +4,7 @@ import { config } from 'dotenv';
 import { ServerlessToolkitStack } from './index';
 
 (async () => {
-	const environment = config({ path: join(process.cwd(), '.env') }).parsed || {
-		...process.env,
-		AWS_REGION: undefined,
-		AWS_ACCOUNT: undefined
-	};
+	const environment = config({ path: join(process.cwd(), '.env') }).parsed || {};
 	const pkg = await import(join(process.cwd(), 'package.json'));
 	const projectName = environment?.PROJECTNAME || pkg?.name?.replace('@', '').replace('/', '-');
 	const domainName = environment?.DOMAINNAME || pkg?.stk?.domainName;
