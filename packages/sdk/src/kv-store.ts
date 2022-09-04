6ae2547f0389c8a0b2e3ad8dde941b85989cf973
Mike Bild <mike@mikebild.com>
2022-09-04 08:54:30 +0200

    update code

--- packages/sdk/src/kv-store.ts
+++ packages/sdk/src/kv-store.ts
@@ -11,17 +11,20 @@ export async function get<T extends Item>(id: string, type: string): Promise<T> 
 		.getItem({
 			TableName: process.env.DBTABLE,
 			ConsistentRead: true,
-			Key: AWS.DynamoDB.Converter.marshall({ id, type })
+			Key: AWS.DynamoDB.Converter.marshall({ id, type }),
 		})
 		.promise();
 	return AWS.DynamoDB.Converter.unmarshall(result.Item) as T;
 }
 
-export async function set<T extends Item>(item: T & { [key: string]: any }, type: string): Promise<void> {
+export async function set<T extends Item>(
+	item: T & { [key: string]: any },
+	type: string
+): Promise<void> {
 	await ddb
 		.putItem({
 			TableName: process.env.DBTABLE,
-			Item: AWS.DynamoDB.Converter.marshall({ type, ...item, context: undefined })
+			Item: AWS.DynamoDB.Converter.marshall({ type, ...item, context: undefined }),
 		})
 		.promise();
 }
